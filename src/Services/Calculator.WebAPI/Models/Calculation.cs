d254c46370bf3c16b155eb4487a097d50d79df8c
peweez <jjalinski@gmail.com>
2020-05-25 16:30:08 +0200

    Create a project structure
    root
      /src
        /Services
          /ServiceA
          /ServiceB
          ...
          /test
        /WebUI
          /SPA
          /test

---
+++ src/Services/Calculator.WebAPI/Models/Calculation.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Calculator.WebAPI.Models
+{
+    public class Calculation
+    {
+        [Key] 
+        public int CalculationID { get; set; }
+        [Required] 
+        public string Owner { get; set; }
+    }
+}
---
+++ src/Services/Calculator.WebAPI/Models/CalculationTask.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calculator.WebAPI.Models
+{
+    public class CalculationTask
+    {
+        [Key]
+        public int CalculationTaskID { get; set; }
+        [ForeignKey(nameof(Models.Task))] 
+        public int TaskID { get; set; }
+        [ForeignKey(nameof(Calculation))]
+        public int CalculationID { get; set; }
+
+        public Task Task { get; set; }
+        public Calculation Calculation { get; set; }
+    }
+}
