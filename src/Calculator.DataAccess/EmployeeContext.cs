e167efaa2440880a8dffc357e57538fd3b3f1c49
Peweez <jjalinski@gmail.com>
2020-06-27 22:51:01 +0200

    fix the build caused by a broken .sln file while changing the architecture

--- src/Calculator.DataAccess/EmployeeContext.cs
+++ src/Calculator.DataAccess/EmployeeContext.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Claims;
 using System.Text;
+using Microsoft.EntityFrameworkCore;
 
 namespace Calculator.DataAccess
 {
-    public class EmployeeContext : DbContext
+    public class EmployeeContext : DbContext, ISupportUser
     {
+
+        public ClaimsPrincipal User { get; set; }
     }
 }
---
+++ src/Calculator/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Calculator.Server.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calculator.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
