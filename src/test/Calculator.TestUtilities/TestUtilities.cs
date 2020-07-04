254ace9b2b66d470b40a56b82c28dd6f75994027
Peweez <jjalinski@gmail.com>
2020-07-05 01:26:17 +0200

    implement a .NET Standard 2.0 Lib with TestUtilities which can be used to help writing tests
    - initial project creation
    - implementation of ConsoleAndFileLogger (credits to Microsoft)

---
+++ src/test/Calculator.TestUtilities/TestUtilities.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Calculator.TestUtilities
+{
+    /// <summary>
+    /// A static class for helper functions
+    /// </summary>
+    public static class TestUtilities
+    {
+        public static string GetTemporaryDirectoryPath()
+        {
+            return Environment.ExpandEnvironmentVariables($@"%TEMP%\{nameof(Calculator.TestUtilities)}");
+        }
+    }
+}
