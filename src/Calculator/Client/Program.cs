7cd4c7f3412d74cf5080047f7fe247d7d95e7188
p3weez <jjalinski@gmail.com>
2020-07-08 19:33:36 +0200

    migrate the .NET Standard 2.0 projects to version 2.1 - add the DevExpress NuGet Server to the NuGet.config - add the DevExpress file link to the index.html - finish EmployeeListHeader.razor - add EmployeeListRow.razor - finish the employee.css stylesheet - add the usage of the DevExpress controls to the DI Service

--- src/Calculator/Client/Program.cs
+++ src/Calculator/Client/Program.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Net.Http;
 using System.Collections.Generic;
+using System.Security.Claims;
 using System.Text;
 using Calculator.BaseRepository;
 using Calculator.Client.Data;
+using Calculator.Controls.Grid;
 using Calculator.Model;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -24,17 +26,28 @@ namespace Calculator.Client
 
             builder.Services.AddHttpClient(BaseClient, client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
                 .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
-
+            
             // Supply HttpClient instances that include access tokens when making requests to the server project
-            builder.Services.AddTransient(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient(BaseClient));
+            builder.Services.AddTransient(sp => 
+                sp.GetRequiredService<IHttpClientFactory>().CreateClient(BaseClient));
 
             builder.Services.AddApiAuthorization();
 
             // client implementation
             builder.Services.AddScoped<IBasicRepository<Employee>, WasmRepository>();
             builder.Services.AddScoped<IUnitOfWork<Employee>, WasmUnitOfWork>();
+            
+            // Add DevExpress Blazor Controls
+            builder.Services.AddDevExpressBlazor();
 
+            // references to control filters and sorts
+            builder.Services.AddScoped<IPageHelper, PageHelper>();
+            builder.Services.AddScoped<IEmployeeFilters, EmployeeFilters>();
+            builder.Services.AddScoped<GridQueryAdapter>();
+            // builder.Services.AddScoped<EditService>(); TODO: implement the EditService
 
+            builder.Services.AddScoped(sp =>
+                new ClaimsPrincipal(new ClaimsIdentity()));
 
             await builder.Build().RunAsync();
         }
